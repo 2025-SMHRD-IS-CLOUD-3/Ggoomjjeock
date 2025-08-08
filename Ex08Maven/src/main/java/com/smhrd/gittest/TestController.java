d73831c4474e7859a154e9ad1627169cf0ea1733
SMHRD <SMHRD@192.168.219.47>
2025-08-08 11:11:44 +0900

    야호

--- Ex08Maven/src/main/java/com/smhrd/gittest/TestController.java
+++ Ex08Maven/src/main/java/com/smhrd/gittest/TestController.java
@@ -4,6 +4,7 @@ package com.smhrd.gittest;
 public class TestController {
 		public static void main(String[] args) {
 			System.out.println("테스트 코드");
-			System.out.println("123123");
+			System.out.println("야ㄴ호");
+			System.out.println("11시 06분");
 		}
 }
