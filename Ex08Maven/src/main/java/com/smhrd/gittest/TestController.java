912f7add5264c60efad5bf51c2b54219e591790c
SMHRD <SMHRD@192.168.219.47>
2025-08-08 11:30:04 +0900

    Merge branch 'main' of https://github.com/2025-SMHRD-IS-CLOUD-3/Ggoomjjeock.git into main

--- Ex08Maven/src/main/java/com/smhrd/gittest/TestController.java
+++ Ex08Maven/src/main/java/com/smhrd/gittest/TestController.java
@@ -4,11 +4,8 @@ package com.smhrd.gittest;
 public class TestController {
 		public static void main(String[] args) {
 			System.out.println("테스트 코드");
-
-			System.out.println("123123");
-			System.out.println("상연코드추가");
-
-			System.out.println("야ㄴ호");
+			System.out.println("야ㄴㄴ호");
 			System.out.println("11시 06분");
+			System.out.println("11시 30분");
 		}
 }
